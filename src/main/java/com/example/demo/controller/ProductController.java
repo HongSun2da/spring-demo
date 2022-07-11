97cc1d42a9abda51cd29b7f53f5b51437914788d
HongSun <hongsun2da@gmail.com>
2022-07-11 17:25:42 +0900

    logback and validation

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,24 +4,31 @@ import com.example.demo.data.dto.ProductDto;
 import com.example.demo.service.ProductService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     @Autowired
     public ProductController(ProductService productService) {
+
         this.productService = productService;
     }
 
     // http://localhost:8080/api/v1/product-api/product/{productId}
     @GetMapping(value = "/product/{productId}")
     public ProductDto getProduct(@PathVariable String productId) {
+        LOGGER.info("[ProductController] Api {}", "getProduct return ProductDto");
         return productService.getProduct(productId);
     }
 
@@ -30,12 +37,14 @@ public class ProductController {
 //            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
 //    })
     @PostMapping(value = "/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+    public ProductDto createProduct(@Valid @RequestBody ProductDto productDto) {
 
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
+
+        LOGGER.info("[createProduct] Response : {}, {}, {}, {}", productId, productName, productPrice, productStock);
 
         return productService.saveProduct(productId, productName, productPrice, productStock);
     }
