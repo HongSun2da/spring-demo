88f5cfb1c39bcd1a677b60db2c2e8e873652bb2c
HongSun <hongsun2da@gmail.com>
2022-07-11 14:47:23 +0900

    main start

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
